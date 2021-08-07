36ede3ba518a8fa81aaf70748d063db41d745e69
ronprscl <73469980+ronprscl@users.noreply.github.com>
2021-08-07 18:53:37 +0300

    main

---
+++ SpeechToText_AppleAPI/Assets/VoiceController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextSpeech;
+using UnityEngine.UI;
+using UnityEngine.Android;
+
+public class VoiceController : MonoBehaviour
+{
+    const string LANG_CODE = "en-US";
+    [SerializeField] Text text;
+    private void Start()
+    {
+        Setup(LANG_CODE);
+        SpeechToText.instance.onResultCallback = OnFinalSpeechResult;
+        CheckPermission();
+    }
+    void Setup(string code)
+    {
+        SpeechToText.instance.Setting(code);
+    }
+    public void StartListening()
+    {
+        SpeechToText.instance.StartRecording();
+        
+    }
+    public void StopListening()
+    {
+        SpeechToText.instance.StopRecording();
+    }
+
+    void OnFinalSpeechResult(string result)
+    {
+        text.text = result;
+    }
+
+    void CheckPermission()
+    {
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+#endif
+    }
+
+   
+
+}
